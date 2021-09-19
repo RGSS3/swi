c8e52dcb4d3d8a754f5586a532619d540849e963
seiran <924172963@qq.com>
2021-09-20 03:47:04 +0800

    Update 2.c

--- test/2.c
+++ test/2.c
@@ -1,21 +1,24 @@
 #define SWI_OPENGL
 #include <6.0g/swi.h>
-#include <gl/gl.h>
-#include <gl/glu.h>
-float a = 0;
+#include <math.h>
+int x = 0;
 void render(void) {
-	a += 0.1f;
 	glClearColor(0.4f, 0.8f, 1.f, 1.f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glBegin(GL_TRIANGLES);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(a, 0);
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glVertex2f(a + 100, 0);
-	glColor3f(0.0f, 0.0f, 1.0f);
-	glVertex2f(a + 100, 100);
+	glBegin(GL_LINES);
+	double p = 2 * M_PI / 3600;
+	x+=10;
+	for (int i = x; i < 1200 + x; ++i) {
+		double x1 = 320 + cos(i * p) * 40;
+		double y1 = 240 + sin(i * p) * 40;
+		double x2 = 320 + cos((i + 1) * p) * 40;
+		double y2 = 240 + sin((i + 1) * p) * 40;
+		glColor3f(1, 0, 0);
+		glVertex2f((float)x1, (float)y1);
+		glColor3f(1, 0, 0);
+		glVertex2f((float)x2, (float)y2);
+	}
 	glEnd();
-	if (a > 640) a = 0;
 }
 
 int main(void) {
@@ -23,14 +26,7 @@ int main(void) {
 	setSize(h, 640, 480);
 	setPos(h, 200, 200);
 	setVisible(h, true);
-
-    glViewport(0, 0, 640, 480);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, 640, 0, 480);
-    glMatrixMode(GL_MODELVIEW);  
-    glLoadIdentity(); 
-    
+	glFlat(640, 480);
 	while (1) {
 		MSG msg;
 		if (poll(&msg)) {
@@ -41,7 +37,6 @@ int main(void) {
 		} else {
 			render();
 			swapBuffers(h);
-			Sleep(10);
 		}
 	}
 }
